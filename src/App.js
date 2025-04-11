b5c4eacf30cf7bd10f57c9b65dce980a27041c0a
Nirjesh <nirjesh64@gmail.com>
2025-04-11 07:58:10 +0530

    fix browserouter issue

--- src/App.js
+++ src/App.js
@@ -1,33 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 import TutorList from './components/Dashboard/TutorList';
 import TutorRegistration from './components/Tutor/TutorRegistration';
 
-
 function App() {
-    return (
-       
-<BrowserRouter>
-            <Routes>
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/tutorlist" element={<TutorList />} />
-                <Route path="/become-tutor" element={<TutorRegistration />} />
-                
-                {/* Add other routes */}
-            </Routes>
-            </BrowserRouter>
-   
-
-
-        
-     
-
-
-
-       
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/tutorlist" element={<TutorList />} />
+        <Route path="/become-tutor" element={<TutorRegistration />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
